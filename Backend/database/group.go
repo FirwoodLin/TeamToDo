ec929b8103add90a9232692478b5e5d323e5ec69
FirwoodLin <tan90_1697@163.com>
2023-07-19 13:50:55 +0800

    feat(model)群组功能完善，任务功能开启

--- Backend/database/group.go
+++ Backend/database/group.go
@@ -25,15 +25,8 @@ func GroupCreate(groupReq *request.GroupCreateRequest) (*model.Group, error) {
 	return &group, err
 }
 
-
-// CheckUserInGroup 检查用户是否在群组中（附带检查用户在群组中的身份）
-func CheckUserInGroup(userID uint, groupID uint) model.Role {
-	var userGroup model.UserGroup
-	_ = global.Sql.Where("user_id = ? AND group_id = ?", userID, groupID).First(&userGroup)
-	return userGroup.Role
-}
-
 // FindUserJoinedGroups 查看个人的群组；查找用户ID加入的所有群组
+// 使用中 暂时不修改参数类型
 func FindUserJoinedGroups(userID uint) (*[]model.UserGroup, error) {
 	db := global.Sql
 	var userGroups []model.UserGroup
@@ -45,17 +38,17 @@ func FindUserJoinedGroups(userID uint) (*[]model.UserGroup, error) {
 }
 
 // FindGroupMembers 查找群组中的所有成员
-func FindGroupMembers(groupID uint) (*[]model.UserGroup, error) {
+func FindGroupMembers(groupID uint) (userGroups []model.UserGroup, err error) {
 	db := global.Sql
-	var userGroups []model.UserGroup
 	if err := db.Where("groupID = ?", groupID).Preload("User").Find(&userGroups).Error; err != nil {
 		// 处理错误
 		global.Logger.Infof("查找群组中的所有成员时,数据库错误\n")
+		return nil, err
 	}
-	return &userGroups, nil
+	return userGroups, nil
 }
 
-// AddGroupMember 添加群组成员 - 服务层调用
+// AddGroupMember 添加群组成员 - 服务层调用 - 在UserGroup表中添加一条记录
 func AddGroupMember(userID uint, groupID uint) error {
 	db := global.Sql
 	var userGroup model.UserGroup
@@ -65,6 +58,41 @@ func AddGroupMember(userID uint, groupID uint) error {
 	if err := db.Create(&userGroup).Error; err != nil {
 		// 处理错误
 		global.Logger.Infof("添加群组成员时,数据库错误\n")
+		return err
+	}
+	return nil
+}
+
+// UpdateMemberRole 更新成员角色（不包括踢出）
+func UpdateMemberRole(userID uint, groupID uint, role model.Role) error {
+	db := global.Sql
+	var userGroup model.UserGroup
+	if err := db.Where("userID = ? AND groupID = ?", userID, groupID).First(&userGroup).Error; err != nil {
+		// 处理错误
+		global.Logger.Infof("更新成员状态时,数据库错误，查询不到记录\n")
+		return err
+	}
+	userGroup.Role = role
+	if err := db.Save(&userGroup).Error; err != nil {
+		// 处理错误
+		global.Logger.Infof("更新成员状态时,数据库错误\n")
+		return err
+	}
+	return nil
+}
+
+// DeleteGroupMember 删除群组成员 - 踢出成员/退出群组通用
+func DeleteGroupMember(userID uint, groupID uint) error {
+	db := global.Sql
+	var userGroup model.UserGroup
+	if err := db.Where("userID = ? AND groupID = ?", userID, groupID).First(&userGroup).Error; err != nil {
+		// 处理错误
+		global.Logger.Infof("删除群组成员时,数据库错误，查询不到记录\n")
+		return err
+	}
+	if err := db.Delete(&userGroup).Error; err != nil {
+		// 处理错误
+		global.Logger.Infof("删除群组成员时,数据库错误\n")
 		return err
 	}
 	return nil
