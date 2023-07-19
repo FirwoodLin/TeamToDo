40ae235b6549c78d70502b19033fc47c9cd1c537
BigWang <2972437973@qq.com>
2023-07-19 19:05:44 +0800

    add global.js and team selector

---
+++ Frontend/JS/task.js
@@ -0,0 +1,31 @@
+(function() {
+    const taskModal = document.querySelector('.task-info-modal');
+    const createTaskBtn = document.querySelector('.frame');
+    const statusBtn = document.querySelector('.task-complete');
+    const textArea = document.querySelector('.task-info-modal textarea');
+    
+    //文本编辑模块框打开
+    $(function() {
+        $('#open-task-modal').click(function(event) {
+          event.preventDefault();
+          $('#task-info-modal').modal({
+            modalClass: 'task-info-modal'
+          });
+        });
+      });
+
+    //文本编辑模块框关闭
+    $(function() {
+        $('.save-task').click(function(event) {
+          event.preventDefault();
+          $.modal.close();
+        });
+      });
+
+      //自动调整文本编辑框大小
+    textArea.addEventListener('input', function() {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+
+})();
