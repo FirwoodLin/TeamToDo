eec33a7e8fb730122f9dbf06635f8b1f43ba9899
FirwoodLin <tan90_1697@163.com>
2023-07-19 20:04:03 +0800

    bugfix:调试群组模块

--- Backend/model/group.go
+++ Backend/model/group.go
@@ -5,27 +5,27 @@ type Role int
 // 定义角色的方式
 const (
 	RoleVisitor Role = iota //  RoleVisitor 不在群组中 （被踢出）
-	RoleOwner
-	RoleAdmin
 	RoleMember
+	RoleAdmin
+	RoleOwner
 )
 
 type Group struct {
 	TimeModel
-	GroupID     uint        `json:"groupID" gorm:"column:groupID;primaryKey"`
-	Description string      `json:"description" gorm:"column:description"`
-	GroupName   string      `json:"groupName"  gorm:"column:groupName"` // 群组名称
-	GroupAvatar string      `json:"groupAvatar" gorm:"column:groupAvatar"`
-	MemberCount int         `json:"memberCount" gorm:"column:description"`
-	Members     []UserGroup `gorm:"foreignKey:groupID"`
+	GroupID     uint   `json:"groupID" gorm:"column:groupID;primaryKey"`
+	Description string `json:"description" gorm:"column:description"`
+	GroupName   string `json:"groupName"  gorm:"column:groupName"` // 群组名称
+	GroupAvatar string `json:"groupAvatar" gorm:"column:groupAvatar"`
+	MemberCount int    `json:"memberCount" gorm:"column:description"`
+	//Members     []UserGroup `gorm:"foreignKey:groupID"`
 }
 
 // UserGroup 用户-群组关系表；记录用户加入的群组
 type UserGroup struct {
 	TimeModel
 	// 联合主键
-	UserID  uint  `gorm:"primaryKey"`
-	GroupID uint  `gorm:"primaryKey"`
+	UserID  uint  `gorm:"primaryKey;column:userID"`
+	GroupID uint  `gorm:"primaryKey;column:groupID"`
 	Role    Role  `gorm:"column:role"`
 	User    User  `gorm:"foreignKey:userID"`
 	Group   Group `gorm:"foreignKey:groupID"`
