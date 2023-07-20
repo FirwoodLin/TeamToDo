7758d1b35096d934cbd95b1552415148d0932c7c
BigWang <2972437973@qq.com>
2023-07-21 00:31:35 +0800

    写了一部分任务列表更新,用户列表更新函数

--- Frontend/JS/user_list.js
+++ Frontend/JS/user_list.js
@@ -1,10 +1,12 @@
-import { token, currentGroupID, getGroupIDs, getGroupNames, getGroupMemberNames, getGroupMemberIDs, getGroupMemberAvatars,
+import {token, currentGroupID,currentUserID, UserID, getGroupIDs, getGroupNames, getGroupMemberNames, getGroupMemberIDs, getGroupMemberAvatars,
     getTaskNames, getTaskIDs, getTaskDescriptions, getTaskStatuses, getTaskDeadlines, getTaskStartAts, convertDateTimeFormat,
-     formatDateTimeLocal, updateSelectOptions, updateGroupMembersList, updateTaskList} from "./global.js";
+     formatDateTimeLocal, updateSelectOptions, updateGroupMembersList, updateTaskList, getUserRole, formatDateTimeLocalToClient} from "./global.js";
 
 (function() {
     const confirmBtn = document.querySelector('.select-team input');
-
+    const localUser = document.querySelector('.frame-parent div .user-avatar');
+    const otherUser = document.querySelector('.instance-parent');
+    const selectedID = 'selected';
 
 
     confirmBtn.addEventListener('click', function(event) {
@@ -13,16 +15,44 @@ import { token, currentGroupID, getGroupIDs, getGroupNames, getGroupMemberNames,
     let select = document.getElementById('teams');
     let selectedValue = select.value;
     currentGroupID = selectedValue;
-        //获取当前组的成员ID
-    let groupMemberIDs = getGroupMemberIDs(selectedValue);
         //更新成员列表
     updateGroupMembersList(currentGroupID);
         //更新任务列表
-    updateTaskList(currentGroupID, );
+    updateTaskList(currentGroupID, currentUserID);
      });
 
+     otherUser.addEventListener('click', function(event) {
+       if (event.target.classList.contains('user-avatar')) {
+          var userInstance = event.target;
+          while (userInstance && !userInstance.classList.contains('user-instance')) {
+            userInstance = userInstance.parentElement;
+          }
+          if (userInstance) {
+            var userNameElement = userInstance.querySelector('.user-name');
+            currentUserID = userNameElement.value;
+            var previousSelected = document.getElementById(selectedID);
+            if (previousSelected) {
+              previousSelected.removeAttribute('id');
+            }
+            userInstance.id = selectedID;
+            updateTaskList(currentGroupID, currentUserID);
+          }
+        }
+      });
+      
+        localUser.addEventListener('click', function(event) {
+            let frameParent = event.target.parentElement;
+            frameParent = frameParent.parentElement;
 
-
+            let userNameElement = frameParent.querySelector('.user-name');
+            currentUserID = userNameElement.value;
+            var previousSelected = document.getElementById(selectedID);
+            if (previousSelected) {
+                previousSelected.removeAttribute('id');
+            }
+            frameParent.id = selectedID;
+            updateTaskList(currentGroupID, currentUserID);
+        });
 
 
 })();
